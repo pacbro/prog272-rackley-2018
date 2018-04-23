04f39a4e16e66f4b10b21511d98db2ac0decdb0e
Jordan R <jordan.rackley@bellevuecollege.edu>
2018-04-23 08:36:38 +0000

    Finished Unit test with Address assignment

--- week02-rest-basics/src/App.js
+++ week02-rest-basics/src/App.js
@@ -4,15 +4,31 @@ import './App.css';
 
 class App extends Component {
   constructor() {
-       super();
-       this.state = {
-   file: 'unknown',
-   foo: 'waiting'
-}}
-getFile = () => {
-   console.log('getFile called.');
-   this.setState({file: 'url-file.js'})
-};
+    super();
+    this.state = {
+      file: 'unknown',
+      firstName: 'unknown',
+      lastName: 'unknown',
+      address: 'unknown',
+      city: 'unknown',
+      state: 'unknown',
+      zip: 'unknown',
+    }
+  }
+  getFileName = () => {
+    console.log('getFileName called.');
+    this.setState({ file: 'url-file.js' })
+  };
+  setAddress = () => {
+    this.setState({
+      firstName: 'Patty',
+      lastName: 'Murray',
+      address: '915 2nd Ave # 2988',
+      city: 'Seattle',
+      state: 'WA',
+      zip: '98174'
+    })
+  };
   render() {
     return (
       <div className="App">
@@ -21,10 +37,17 @@ getFile = () => {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">file: {this.state.file}</p>
+        <p className="App-intro">firstName: {this.state.firstName}</p>
+        <p className="App-intro">lastName: {this.state.lastName}</p>
+        <p className="App-intro">address: {this.state.address}</p>
+        <p className="App-intro">city: {this.state.city}</p>
+        <p className="App-intro">state: {this.state.state}</p>
+        <p className="App-intro">zip: {this.state.zip}</p>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <button id='getFile' onClick={this.getFile}>Get File</button>
+        <button id='getFile' onClick={this.getFileName}>Get File</button>
+        <button className="setAddress" onClick={this.setAddress}>Set Address</button>
       </div>
     );
   }
--- week02-rest-basics/src/App.test.js
+++ week02-rest-basics/src/App.test.js
@@ -1,28 +1,149 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {configure, shallow} from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
 describe('jest test', function() {
 
    it('renders without crashing', () => {
-       const div = document.createElement('div');
-       ReactDOM.render(<App />, div);
+      const div = document.createElement('div');
+      ReactDOM.render(<App />, div);
    });
 
    it('renders and reads H1 text', () => {
-       const wrapper = shallow(<App />);
-       const welcome = <h1 className="App-title">Welcome to React</h1>;
-       expect(wrapper.contains(welcome)).toEqual(true);
+      const wrapper = shallow(<App />);
+      const welcome = <h1 className="App-title">Welcome to React</h1>;
+      const h1Data = wrapper.find('header').childAt(1).debug();
+      console.log(h1Data);
+      expect(wrapper.contains(welcome)).toEqual(true);
    });
-   
-   it('renders state of File paragraph after button click', () => {
-   const wrapper = shallow(<App />);
-   const nineSign = <p className="App-intro">file: url-file.js</p>;
-   wrapper.find('#getFile').simulate('click');
-   expect(wrapper.contains(nineSign)).toEqual(true);
-});
 
+   it('renders and reads paragraph text', () => {
+      const wrapper = shallow(<App />);
+      const unknownFile = <p className="App-intro">file: unknown</p>;
+      const paragraph = wrapper.find('div').childAt(1).debug();
+      console.log(paragraph);
+      expect(wrapper.contains(unknownFile)).toEqual(true);
+   });
+
+   it('renders state of File paragraph after button click', () => {
+      const wrapper = shallow(<App />);
+      const fileSign = <p className="App-intro">file: url-file.js</p>;
+      wrapper.find('#getFile').simulate('click');
+      const paragraphData = wrapper.find('div').childAt(1).debug();
+      console.log(paragraphData);
+      expect(wrapper.contains(fileSign)).toBe(true);
+   });
+
+   it('renders and displays the default first name', () => {
+      const wrapper = shallow(<App />);
+      const welcome = <p className="App-intro">firstName: unknown</p>;
+      const lastParagraph = wrapper.find('p').last().debug();
+      console.log(lastParagraph);
+      expect(wrapper.contains(welcome)).toEqual(true);
+   });
+
+   it('renders and displays the default first name', () => {
+      const wrapper = shallow(<App />);
+      const welcome = <p className="App-intro">firstName: unknown</p>;
+      const lastParagraph = wrapper.find('p').last().debug();
+      console.log(lastParagraph);
+      expect(wrapper.contains(welcome)).toEqual(true);
+   });
+
+   it('renders and displays the default last name', () => {
+      const wrapper = shallow(<App />);
+      const welcome = <p className="App-intro">lastName: unknown</p>
+      const lastParagraph = wrapper.find('p').last().debug();
+      console.log(lastParagraph);
+      expect(wrapper.contains(welcome)).toEqual(true);
+   });
+
+   it('renders and displays the default address', () => {
+      const wrapper = shallow(<App />);
+      const welcome = <p className="App-intro">address: unknown</p>
+      const lastParagraph = wrapper.find('p').last().debug();
+      console.log(lastParagraph);
+      expect(wrapper.contains(welcome)).toEqual(true);
+   });
+
+   it('renders and displays the default city', () => {
+      const wrapper = shallow(<App />);
+      const welcome = <p className="App-intro">city: unknown</p>
+      const lastParagraph = wrapper.find('p').last().debug();
+      console.log(lastParagraph);
+      expect(wrapper.contains(welcome)).toEqual(true);
+   });
+
+   it('renders and displays the default state', () => {
+      const wrapper = shallow(<App />);
+      const welcome = <p className="App-intro">state: unknown</p>
+      const lastParagraph = wrapper.find('p').last().debug();
+      console.log(lastParagraph);
+      expect(wrapper.contains(welcome)).toEqual(true);
+   });
+
+   it('renders and displays the default zip', () => {
+      const wrapper = shallow(<App />);
+      const welcome = <p className="App-intro">zip: unknown</p>
+      const lastParagraph = wrapper.find('p').last().debug();
+      console.log(lastParagraph);
+      expect(wrapper.contains(welcome)).toEqual(true);
+   });
+
+   it('renders and displays the first name after setAddress button click', () => {
+      const wrapper = shallow(<App />);
+      wrapper.find('.setAddress').simulate('click');
+      const welcome = <p className="App-intro">firstName: Patty</p>;
+      const lastParagraph = wrapper.find('p').last().debug();
+      console.log(lastParagraph);
+      expect(wrapper.contains(welcome)).toEqual(true);
+   });
+
+   it('renders and displays the last name after setAddress button click', () => {
+      const wrapper = shallow(<App />);
+      wrapper.find('.setAddress').simulate('click');
+      const welcome = <p className="App-intro">lastName: Murray</p>
+      const lastParagraph = wrapper.find('p').last().debug();
+      console.log(lastParagraph);
+      expect(wrapper.contains(welcome)).toEqual(true);
+   });
+
+   it('renders and displays the address after setAddress button click', () => {
+      const wrapper = shallow(<App />);
+      wrapper.find('.setAddress').simulate('click');
+      const welcome = <p className="App-intro">address: 915 2nd Ave # 2988</p>
+      const lastParagraph = wrapper.find('p').last().debug();
+      console.log(lastParagraph);
+      expect(wrapper.contains(welcome)).toEqual(true);
+   });
+
+   it('renders and displays the default city after setAddress button click', () => {
+      const wrapper = shallow(<App />);
+      wrapper.find('.setAddress').simulate('click');
+      const welcome = <p className="App-intro">city: Seattle</p>
+      const lastParagraph = wrapper.find('p').last().debug();
+      console.log(lastParagraph);
+      expect(wrapper.contains(welcome)).toEqual(true);
+   });
+
+   it('renders and displays the default state after setAddress button click', () => {
+      const wrapper = shallow(<App />);
+      wrapper.find('.setAddress').simulate('click');
+      const welcome = <p className="App-intro">state: WA</p>
+      const lastParagraph = wrapper.find('p').last().debug();
+      console.log(lastParagraph);
+      expect(wrapper.contains(welcome)).toEqual(true);
+   });
+
+   it('renders and displays the default zip after setAddress button click', () => {
+      const wrapper = shallow(<App />);
+      wrapper.find('.setAddress').simulate('click');
+      const welcome = <p className="App-intro">zip: 98174</p>
+      const lastParagraph = wrapper.find('p').last().debug();
+      console.log(lastParagraph);
+      expect(wrapper.contains(welcome)).toEqual(true);
+   });
 });
