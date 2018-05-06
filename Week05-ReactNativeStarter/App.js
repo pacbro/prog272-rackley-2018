0bf669ab352305756851175214b1cad4425235aa
Your Name <you@example.com>
2018-05-05 18:19:09 -0700

    finished react native expo assignment

--- Week05-ReactNativeStarter/App.js
+++ Week05-ReactNativeStarter/App.js
@@ -1,23 +1,67 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {
+    Button,
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+export default class ReactNativeStarter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            foo: 'expo try state'
+        };
+
+    }
+
+    fetchAddress = () => {
+        //this.setState({foo:'fetcher king'});
+        const that = this;
+        const ip = 'ccalvert.com';
+        fetch('http://' + ip + ':30027/you-rang')
+            .then((response) => response.json())
+            .then(function(result) {
+                this.collection = result.allData;
+                that.setState({
+                    foo: result.result,
+                });
+            }).catch(function(ex) {
+                that.setState({foo: 'qux error'});
+            });
+    };
+
+    render() {
+        console.log('tst');
+
+        return (
+            <View style={styles.container}>
+                <Text>Getting started now</Text>
+                <Text style={styles.instructions}>
+                    {this.state.foo}
+                </Text>
+
+                <Button
+                    onPress={this.fetchAddress}
+                    title="Get Data"
+                    color="#841584"
+                    accessibilityLabel="Learn more about this purple button"
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
+
+
+AppRegistry.registerComponent('ReactNativeStarter', () => ReactNativeStarter);
+
