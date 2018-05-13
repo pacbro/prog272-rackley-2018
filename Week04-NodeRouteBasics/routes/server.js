a5ecdb3620bf5b8cc645a6e0c296e2d0b566da07
Jordan R <jordan.rackley@bellevuecollege.edu>
2018-05-13 21:12:52 +0000

    finished noderoutes

---
+++ Week04-NodeRouteBasics/routes/server.js
@@ -0,0 +1,9 @@
+
+           router.get('/calculateFeetFromMiles', function(request, response) {
+    response.send({result: request.query.miles * 5280});
+});
+
+router.post('/calculateCircumference', function(request, response) {
+    console.log(request.body);
+    response.send({result: request.query.radius * 2 * 3.14});
+});
